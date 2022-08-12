c34a47ee6f72aebec371f5a4c315a30fa6933907
phibieroberts <phibierobert@gmail.com>
2022-08-12 01:07:16 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ParticleBackground from "./ParticleBackground";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticleBackground />
+      <h1>particlejs</h1>
+      <br></br>
+      <h2>lets try pushing this react app to github for the very first time</h2>
     </div>
   );
 }
